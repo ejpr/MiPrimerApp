15f8a76c4261c90b5441cd1fe62232b935eaa66c
Unknown <eduardo28709@gmail.com>
2018-09-25 20:37:18 -0300

    ImageView, Fry meme

--- app/src/main/java/com/example/digital/miprimerapp/MainActivity.java
+++ app/src/main/java/com/example/digital/miprimerapp/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void accionClickMe(View view){
+        Toast.makeText(this, "Edu Click", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Te dije que no me clickees -.-", Toast.LENGTH_SHORT).show();
     }
 }
