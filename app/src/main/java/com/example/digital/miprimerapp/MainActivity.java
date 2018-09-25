5aee1974c54cdbd10199f14d889a123517ec2058
Unknown <eduardo28709@gmail.com>
2018-09-25 19:51:53 -0300

    Primer Commit Edu, MI pirmeraAPp

---
+++ app/src/main/java/com/example/digital/miprimerapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.digital.miprimerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void accionClickMe(View view){
+        Toast.makeText(this, "ME CLICLEARON", Toast.LENGTH_SHORT).show();
+    }
+}
